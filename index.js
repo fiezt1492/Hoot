24275296ee93e3f7e88eacd5afe0b98c8f7c6f6c
Fiezt <dat09t@gmail.com>
2022-07-04 14:30:37 +0700

    add shard manager

---
+++ index.js
@@ -0,0 +1,16 @@
+const { ShardingManager } = require('discord.js');
+const { token, dev } = require("./config.js");
+const manager = new ShardingManager('./bot.js', { token: token });
+
+manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
+
+manager.spawn();
+
+// if (dev !== "on") {
+//     const { AutoPoster } = require('topgg-autoposter')
+//     const ap = AutoPoster(topggToken, manager)
+
+//     ap.on('posted', () => {
+//       console.log('Posted stats to Top.gg!')
+//     })
+// }
