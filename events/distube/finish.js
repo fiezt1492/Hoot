a0f329eebf7c4fbe8c8028188045deccd980258a
Fiezt <dat09t@gmail.com>
2022-07-05 10:12:34 +0700

    fix

--- events/distube/finish.js
+++ events/distube/finish.js
@@ -1,20 +1,6 @@
 const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-	name: "error",
-	execute(channel, e, client, status) {
-		if (channel)
-			channel.send({
-				embeds: [
-					new MessageEmbed()
-						.setColor("RED")
-						.setTitle("An error encountered")
-						.setAuthor({
-							name: `ERROR`,
-						})
-						.setDescription(`${e.toString().slice(0, 1974)}`),
-				],
-			});
-		else console.error(e);
-	},
+	name: "finish",
+	execute(queue, client, status) {},
 };
