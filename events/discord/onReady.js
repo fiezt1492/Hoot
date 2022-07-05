bf624c56ca230224e451a6022134c8e7f784596f
Fiezt <dat09t@gmail.com>
2022-07-05 10:37:30 +0700

    update

--- events/discord/onReady.js
+++ events/discord/onReady.js
@@ -1,7 +1,10 @@
+const { ActivityType } = require("discord-api-types/v10");
+
 module.exports = {
 	name: "ready",
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+		client.user.setActivity("music", { type: ActivityType.Listening });
 	},
 };
--- events/distube/addList.js
+++ events/distube/addList.js
@@ -8,8 +8,16 @@ module.exports = {
 				new MessageEmbed()
 					.setColor("RANDOM")
 					.setDescription(
-						`Added [\`${playlist.name}\`](${playlist.url}) playlist (\`${playlist.songs.length}\` songs) to queue`
+						`Added [\`${playlist.name}\`](${playlist.url}) playlist (\`${
+							playlist.songs.length
+						}\` songs) to queue for total \`${
+							queue.songs.length - 1
+						}\` songs in queue`
 					)
+					.setAuthor({
+						name: `${playlist.user.tag}`,
+						iconURL: `${playlist.user.displayAvatarURL()}`,
+					})
 					.setFooter({ text: `${status(queue)}` }),
 			],
 		});
