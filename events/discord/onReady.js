19e9172eebd3e1c39b8996bd8cc7c88aabfd77af
Fiezt <dat09t@gmail.com>
2022-07-06 11:17:55 +0700

    added status

--- events/discord/onReady.js
+++ events/discord/onReady.js
@@ -5,6 +5,32 @@ module.exports = {
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
-		client.user.setActivity("music", { type: ActivityType.Listening });
+		setInterval(() => {
+			const statuses = [
+				{
+					name: `${client.guilds.cache.size} servers`,
+					type: ActivityType.Watching,
+				},
+				{
+					name: `${client.channels.cache.size} channels`,
+					type: ActivityType.Watching,
+				},
+				{
+					name: `music`,
+					type: ActivityType.Listening,
+				},
+				{
+					name: `/play`,
+					type: ActivityType.Playing,
+				},
+				{
+					name: `music in ${client.distube.queues.collection.size} servers`,
+					type: ActivityType.Playing,
+				},
+			];
+
+			const status = statuses[Math.floor(Math.random() * statuses.length)];
+			client.user.setActivity(status.name, { type: status.type });
+		}, 7200000);
 	},
 };
